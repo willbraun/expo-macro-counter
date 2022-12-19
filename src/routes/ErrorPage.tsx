63313b6f1518c1fa2aa95d35ff4ada47ec2a2adc
willbraun <williamhbraun1@gmail.com>
2022-12-19 11:23:58 -0700

    made react router link underlayColor transparent

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,19 +1,20 @@
 import { View } from "react-native";
-import { useNavigate } from 'react-router-native';
+import { Link } from 'react-router-native';
 import { Button, Text } from 'react-native-paper';
 import createStyles from "../styles/base";
 
 export default function ErrorPage() {
 	const styles = createStyles();
-	const navigate = useNavigate();
 
 	return (
 		<View style={styles.container}>
 			<Text variant='displayMedium'>Page not found</Text>
 
-			<Button mode='elevated' onPress={() => navigate('/')}>
-                <Text>Back to Today</Text>
-            </Button>
+			<Link to='/' underlayColor='transparent'>
+                <Button mode='elevated'>
+                    <Text>Back to Today</Text>
+                </Button>
+            </Link>
 		</View>
 	);
 }
--- src/routes/Today.tsx
+++ src/routes/Today.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import { useNavigate } from 'react-router-native';
+import { Link } from 'react-router-native';
 import { Text, Button } from 'react-native-paper';
 import './../styles/base.js'
 import createStyles from "./../styles/base.js";
@@ -7,15 +7,18 @@ import createStyles from "./../styles/base.js";
 
 export default function Today() {
     const styles = createStyles();
-    const navigate = useNavigate();
     
     return (
         <View style={styles.container}>
             <Text variant='displayMedium'>Today</Text>
 
-            <Button mode='elevated' onPress={() => navigate('/badlink')}>
-                <Text>This button has a bad link</Text>
-            </Button>
+
+                <Link to='/badlink' underlayColor='transparent'>
+                    <Button mode='elevated'>
+                        <Text>This button has a bad link</Text>
+                    </Button>
+                </Link>
+
             
         </View>
     )
